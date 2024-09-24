020c973b0a10ec1b8bafc2b416ca0448761a8ea4
Junaid Ahmed <124657390+Junaidahmed1993@users.noreply.github.com>
2024-09-24 20:56:34 +0200

    Update Basic_C++.cpp

--- Basic_C++.cpp
+++ Basic_C++.cpp
@@ -1,1 +1,42 @@
+// FUNDAMENTALS OF C++ AND ITS SCRIPT
 
+// 1. Very First Line of Code
+#include <iostream>
+
+int main() {
+    std:: cout << "I like pizza, but burger would be more preffered";
+    return 0;
+    
+}
+
+// Lets try once again
+ #Include <iostream>
+
+int main () {
+  std:: cout << "Hellowrodl";
+  return 0;
+}
+
+// We can also use the end line functoion as well but not required or necessary; here is how we can impliment
+
+#include <iostream>
+int main () {
+  std::cout << "Google world" std::endl;  //So, here we impliment the end line functions.
+  return 0;
+}
+
+// Suppose, we want to write two lines of code
+
+#include <iostream>
+int main () {
+  std::cout << "Helloworld" << "\n";
+  std::cout << "Great, lets practise C++";
+return 0;
+
+// Now, instead of using "ln", we can use endline function like following 
+#include <iostream>
+int main () {
+  std::cout << "Helloworld" << std::endl;
+  std::cout << "Great to meet you" << std::endl;
+  return 0;
+}
